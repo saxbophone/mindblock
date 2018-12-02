95a05c45788a7d24f5f26cf9150465d47ad3512c
Joshua Saxby <joshua.a.saxby@gmail.com>
2018-12-02 00:01:28 +0000

    Add dummy Block class

---
+++ mindblock/Block.hpp
@@ -0,0 +1,20 @@
+#ifndef SAXBOPHONE_MINDBLOCK_BLOCK_HPP
+#define SAXBOPHONE_MINDBLOCK_BLOCK_HPP
+
+namespace mindblock {
+    /**
+     * @brief A block occupying a single tile in a Mindblock puzzle
+     * @details Blocks can have varying colours and shapes and stick to each
+     * other
+     */
+    class Block {
+        public:
+            /**
+             * @brief Default constructor
+             * @details Produces a white square
+             */
+            Block();
+    };
+}
+
+#endif // include guard
--- mindblock/Stub.cpp
+++
@@ -1,1 +0,0 @@
-// NOTE: This is a stub compilation unit used as a placeholder for now

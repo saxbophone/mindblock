d8cfcb6e5cb4f9000fe1bbcb8449c89809a9a1a8
Joshua Saxby <joshua.a.saxby@gmail.com>
2019-05-18 16:28:37 +0100

    Allocate space for level grid

--- mindblock/Level.cpp
+++ mindblock/Level.cpp
@@ -2,5 +2,15 @@
 
 
 namespace mindblock {
-    Level::Level() {}
+    Level::Level(size_t grid_size)
+      : grid_size(grid_size)
+      {
+        // set grid vector to the correct size
+        this->grid.resize(grid_size);
+        // resize vector's columns too
+        for (auto &column : this->grid) {
+            column.resize(grid_size, NULL);
+        }
+        // TODO: randomly generate a puzzle
+    }
 }
--- mindblock/Level.hpp
+++ mindblock/Level.hpp
@@ -2,15 +2,19 @@
 #define SAXBOPHONE_MINDBLOCK_LEVEL_HPP
 
 #include <unordered_set>
+#include <vector>
 
 #include "Block.hpp"
 
 namespace mindblock {
     class Level {
     public:
-        Level();
+        Level(size_t grid_size=7);
     private:
+        // set of all the blocks used in the puzzle
         std::unordered_set<Block, hash_block> block_palette;
+        size_t grid_size; // the size of each grid axis
+        std::vector<std::vector<Block*>> grid; // the game grid
     };
 }
 
